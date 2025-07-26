32702dd51e2e9b1ab21287f4c26b24608d77e56a
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-26 13:37:58 +0330

    Creating Models

--- polls/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index (req) :
-    return HttpResponse("Hello , Im Alireza A Champion Of The Word In Boxing Programming And A Rich Man !!")
