60738936db200fb3b9ecb4683b976fe2959774c3
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-28 11:23:31 +0330

    Writing more views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(req) :
+    return HttpResponse("Hi Im Index Func")
+
+def detail(req , question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def result (req , question_id):
+    return HttpResponse(f"you're looking at the result of question {question_id}")
+
+def vote(req , question_id) :
+    return HttpResponse(f"You're voting on question {question_id}")
+
