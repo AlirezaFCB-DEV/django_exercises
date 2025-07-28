05bf78af9852a88fd0de1208ad14aac0cf13620a
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-28 15:10:54 +0330

    A shortcut: render()

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse # ! Not Good Render That Better Than HttpRes
-from django.template import loader
+from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Question
 # Create your views here.
@@ -8,9 +7,8 @@ from .models import Question
 
 def index(req):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {"latest_question_list" :  latest_question_list}
-    return HttpResponse(template.render(context , req))
+    context = {"latest_question_list":  latest_question_list}
+    return render(req, "polls/index.html", context)
 
 
 def detail(req, question_id):
