506e7c66076322b82f42926935f4ab1685a9651a
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-06 10:25:45 +0330

    create action with mongo

--- shop/polls/views.py
+++ shop/polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from datetime import datetime
 
+from .models import User
 # Create your views here.
+
+user = User(
+    name="Alireza",
+    age= 16,
+    email= "alirezadeveloper5@gmail.com",
+    created_at = datetime.utcnow()
+)
+
+user.save()
