6f11035a2a47a4e97b4c4dc7490f099d449bc175
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-06 10:25:06 +0330

    create first model with mongo

--- shop/polls/models.py
+++ shop/polls/models.py
@@ -1,3 +1,8 @@
-from django.db import models
-
+from mongoengine import Document , StringField , IntField , DateTimeField
 # Create your models here.
+
+class User (Document) :
+    name = StringField(required=True , max_length=50)
+    age = IntField(required=True , max_value=100)
+    email = StringField(required=True , max_length=100)
+    create_at = DateTimeField()
