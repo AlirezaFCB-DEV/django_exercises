1985ff6e16a7883cb6389ec3025a04848d1eb257
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-11 11:31:19 +0330

    start a new project

--- shop/polls/froms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class User_Form(forms.Form):
-    name =  forms.CharField(max_length=50)
-    age = forms.IntegerField()
-    email = forms.EmailField()
--- shop/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import show_users, update_user, delete_user, search_users
-
-urlpatterns = [
-    path("", show_users, name="show_users"),
-    path("update/", update_user, name="update_user"),
-    path("delete-user/", delete_user, name="delete_user"),
-    path("search/", search_users, name="search_users")
-]
