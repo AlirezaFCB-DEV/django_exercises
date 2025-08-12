ee7b44abbd6fc31dfa9b61d55249db8d24acbe42
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-12 13:22:39 +0330

    make more class_models and fileds

--- users/models.py
+++ users/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Person (models) :
+class Person(models.Model) :
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+class Musician(models.Model) :
+    f_name = models.CharField(max_length=50)
+    l_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+    
+class Album(models.Model):
+    artist = models.ForeignKey(Musician , on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_start = models.IntegerField()
     
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse
 
 # Create your views here.
+
+def test_view (req) :
+    return HttpResponse("Hello I'm Test View In Django")
