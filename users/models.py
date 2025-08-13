8a02dfa58b57713359f839c1e32a4eeb60546ddc
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-13 09:45:39 +0330

    filed types

--- users/models.py
+++ users/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model) :
+    SHIRT_SIZES= [
+        ("S" , "Small"),
+        ("M" , "Medium"),
+        ("L" , "Large"),
+        ("XL" , "XLarge"),
+    ]
+    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    shirt_size = models.CharField(max_length=2 , choices=SHIRT_SIZES) 
+    
 
 class Musician(models.Model) :
     f_name = models.CharField(max_length=50)
@@ -15,4 +24,9 @@ class Album(models.Model):
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_start = models.IntegerField()
+    
+    
+class Runner(models.Model) :
+    MedalType = models.TextChoices("MedalType" , "Gold Silver Bronze")
+    name = models.CharField(max_length=60)
     
