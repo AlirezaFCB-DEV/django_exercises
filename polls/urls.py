b952658a1eb5d6eca41e0ff08f5dd5c5e3f9dc28
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-18 17:48:41 +0330

    Returning errors

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import first_view
+from .views import first_view, my_error_view
 
 urlpatterns = [
-    path("" , first_view , name="first_view")
+    path("", first_view, name="first_view"),
+    path("err/", my_error_view, name="my_error_view")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
 
-def first_view (req) :
+
+def first_view(req):
     return HttpResponse("Hello , This first my view!!")
+
+
+def my_error_view(req):
+    if 5 == 0:
+        return HttpResponse("Welcome HAHAHAHA!!")
+    else:
+        return HttpResponseBadRequest("You Req Is NotVALID!!")
