10d610f6fd8e20bc2d57d4fb69ffd2023af7f4f4
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-19 11:22:17 +0330

    Testing custom error views

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
 # Create your tests here.
+
+client = Client()
+
+
+def test_404_page():
+    response = client.get("/polls/404")
+
+    assert response.status_code == 404
