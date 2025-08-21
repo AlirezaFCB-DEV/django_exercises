f6d400360060e4bb0d25a5d3d83527e3ca8e4611
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-08-21 12:45:53 +0330

    Regular Expressions

--- polls/urls.py
+++ polls/urls.py
@@ -1,19 +1,20 @@
-from django.urls import path, register_converter
+from django.urls import path, re_path
+# , register_converter
 
 from .views import first_view, my_error_view, persons_details_view, my_404_view, my_async_view, article_detail, article_year
 
 
-class FourDigitYearConverter:
-    regex = '[0-9]{4}'
+# class FourDigitYearConverter:
+#     regex = '[0-9]{4}'
 
-    def to_python(self, value):
-        return int(value)
+#     def to_python(self, value):
+#         return int(value)
 
-    def to_url(self, value):
-        return str(value)
+#     def to_url(self, value):
+#         return str(value)
 
 
-register_converter(FourDigitYearConverter, "yyyy")
+# register_converter(FourDigitYearConverter, "yyyy")
 
 
 urlpatterns = [
@@ -24,6 +25,7 @@ urlpatterns = [
     path("404/", my_404_view, name="404_error"),
     path("async/", my_async_view, name="async_view"),
     path("articles/<int:article_id>/", article_detail, name="article_detail"),
-    path("articles/year/<yyyy:year>/", article_year)
+    # path("articles/year/<yyyy:year>/", article_year)
+    re_path(r"^articles/(?P<year>[0-9]{4})/$", article_year)
 
 ]
