dd9234cb8728902271281a4be982c1e8fd9554e3
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-09-08 14:10:09 +0330

    Hooks

--- account_exercise/middleware/timing.py
+++ account_exercise/middleware/timing.py
@@ -20,3 +20,11 @@ class Request_Timing_Middleware:
         # ? Add header for watching response time
         response["X-Request-Duration-ms"] = str(int(duration * 1000))
         return response
+
+    def process_view(self, req, view_func, view_args, view_kwargs):
+        # ? We Check If View Other Special Attrs
+        if getattr(view_func, "require_login", False) and not req.user.is_authenticated:
+            from django.http import JsonResponse
+            return JsonResponse({"error": "auth required"}, status=401)
+        #! none means continue processing (other middleware is also checked)
+        return None
