c1577f6be2aa74402d5286bbe61d56b1e4ae9752
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-09-08 14:32:16 +0330

    async middlewares

--- account_exercise/middleware/timing.py
+++ account_exercise/middleware/timing.py
@@ -1,4 +1,7 @@
 import time
+from django.http import StreamingHttpResponse
+import asyncio
+from asgiref.sync import sync_to_async
 
 
 class Request_Timing_Middleware:
@@ -7,18 +10,12 @@ class Request_Timing_Middleware:
         #! This method just one call on startup.
 
     def __call__(self, req):
-        #! This method for all req callable.
-
-        if not self.enabled:
-            return self.get_response(req)
-
-        start = time.time()  # ? Start Time
-        # ? Resume Process To Rest Of Middlewares And In End View
         response = self.get_response(req)
-        duration = time.time() - start  # ? Run Time
 
-        # ? Add header for watching response time
-        response["X-Request-Duration-ms"] = str(int(duration * 1000))
+        if isinstance(response, StreamingHttpResponse):
+            response["X-Streaming"] = "1"
+
+        response["X-Processed"] = "1"
         return response
 
     def process_view(self, req, view_func, view_args, view_kwargs):
@@ -28,13 +25,35 @@ class Request_Timing_Middleware:
             return JsonResponse({"error": "auth required"}, status=401)
         #! none means continue processing (other middleware is also checked)
         return None
-    
-    def process_exception(self , req , exception):
+
+    def process_exception(self, req, exception):
         #! log
-        import logging , traceback
-        
+        import logging
+        import traceback
+
         logging.exception("Unhandled exception in view")
-        
+
         from django.http import JsonResponse
-        
-        return JsonResponse({"error" : "internal server error"} , status=500)
+
+        return JsonResponse({"error": "internal server error"}, status=500)
+
+
+class Async_Request_Logger_Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.is_async = asyncio.iscoroutinefunction(get_response)
+
+    async def __call__(self, req):
+        #!  Pre process (async-safe)
+        start = asyncio.get_event_loop().time()
+
+        if self.is_async:
+            response = await self.get_response(req)
+        else:
+            response = await sync_to_async(self.get_response)(req)
+
+        duration = asyncio.get_event_loop().time() - start
+
+        response["X-Async-Duration-ms"] = str(int(duration * 1000))
+
+        return response
