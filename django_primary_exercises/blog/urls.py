8eece1138cea83efc531ae5fd7578f94efe6e284
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-09-09 14:10:59 +0330

    Start Django With DRF

--- django_primary_exercises/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("posts", views.post_list, name="post_list"),
-    path("posts/<str:post_url>/", views.post_detail, name="post_detail"),
-    path("add_post/", views.add_post , name="add_post")
-]
--- django_primary_exercises/users/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render , HttpResponse
-
-# Create your views here.
-
-def test_view (req) :
-    return HttpResponse("Hello I'm Test View In Django")
